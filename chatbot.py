8af9b177ef7d6bd59652d9df001b77a3d0673807
Yuki Miyake <miyake_yuuki320@yahoo.co.jp>
2024-04-13 09:11:24 +0900

    UI変更

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,21 @@
 import gradio as gr
-import random
 
-def random_response(message, history):
-    return random.choice(["Yes", "No"])
+def yes_man(message, history):
+    if message.endswith("?"):
+        return "Yes"
+    else:
+        return "Ask me anything!"
 
-gr.ChatInterface(random_response).launch()
+gr.ChatInterface(
+    yes_man,
+    chatbot=gr.Chatbot(height=300),
+    textbox=gr.Textbox(placeholder="Ask me a yes or no question", container=False, scale=7),
+    title="Yes Man",
+    description="Ask Yes Man any question",
+    theme="soft",
+    examples=["Hello", "Am I cool?", "Are tomatoes vegetables?"],
+    cache_examples=True,
+    retry_btn=None,
+    undo_btn="Delete Previous",
+    clear_btn="Clear",
+).launch()
